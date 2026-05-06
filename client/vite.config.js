f931013f29bd6100882132e5dc020aea208d4e49
Danylo Kost <1kost.danylo@gmail.com>
2026-05-06 19:35:59 +0300

    vite update 4 :(

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const API_URL = process.env.VITE_API_URL || 'http://localhost:3001';
+
 export default defineConfig({
   plugins: [react()],
   server: { 
